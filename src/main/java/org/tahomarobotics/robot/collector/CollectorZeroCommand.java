0e8338bbecb28a7d581418412baa2b62847b1a46
JosiahTheCoder777 <hjmerrill09@gmail.com>
2024-11-25 13:58:23 -0800

    Worked on day 3

---
+++ src/main/java/org/tahomarobotics/robot/collector/CollectorZeroCommand.java
@@ -0,0 +1,126 @@
+package org.tahomarobotics.robot.collector;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+public class CollectorZeroCommand extends Command {
+
+    private final Collector collector = Collector.getInstance();
+
+    public CollectorZeroCommand() {
+        addRequirements(collector);
+    }
+
+    @Override
+    public void execute() {
+        collector.setDeployVoltage(CollectorConstants.COLLECTOR_ZERO_VOLTAGE);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (collector.getLeftPivotVelocity() > -0.5) {
+            return Math.abs(collector.getLeftDeployVelocity()) < CollectorConstants.COLLECTOR_ZERO_VELOCITY_TOLERANCE
+                    && Math.abs(collector.getRightDeployVelocity()) < CollectorConstants.COLLECTOR_ZERO_VELOCITY_TOLERANCE;
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        collector.zeroDeploys();
+        collector.deployStow();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
